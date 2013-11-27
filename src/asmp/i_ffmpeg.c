0d1df21d49293305aa7ae82a3ab2b4108565a094
Christopher Stones <chris.stones@zoho.com>
2013-11-27 22:16:03 +0000

    Implemented openf, added code to read android asset files.

--- src/asmp/i_ffmpeg.c
+++ src/asmp/i_ffmpeg.c
@@ -1,8 +1,20 @@
 
 /***
  *
- * Stream data from ffmpeg
+ * Using FFMPEG to decode audio data.
  *
+ *      This code supports reading audio from ...
+ *        * files on the filesystem.
+ *        * files in an android APK.
+ *        * files in a rawpak container ( either on filesystem, or android APK )
+ *
+ * example,
+ *
+ *     rh_asmp_itf itf;
+ *     rh_asmp_create_ffmpeg( &itf, ... );
+ *     (*itf)->open ( itf, "sounds/sound0.ogg" );                   // load audio from file sounds/sound0.ogg
+ *     (*itf)->openf( itf, "rh_rawpak_ctx://%p", rh_rawpak_ctx  );  // load audio from a rawpak context.
+ *     (*itf)->open ( itf, "apk://sounds/sound0.ogg");              // load audio from android assest sounds/sound0.ogg
  */
 
 #include<rh_raw_loader.h>
@@ -21,9 +33,16 @@ extern "C" {
 #endif
 /**********************************/
 
+#ifdef __ANDROID__
+	#include<android/asset_manager.h>
+	static int apk_open_avformatctx(rh_asmp_itf self, size_t internalBufferSize, AVFormatContext ** format_ctx );
+#endif
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<pthread.h>
+#include<stdarg.h>
+#include<linux/limits.h>
 
 #include "asmp.h"
 
@@ -47,6 +66,16 @@ struct asmp_instance {
 	int samplesize;
 	pthread_mutex_t monitor;
 	int ref;
+
+#ifdef __ANDROID__
+	struct {
+		void  *avformat_buffer;
+		size_t avformat_buffer_size;
+		AAsset* asset;
+		AAssetManager * assetManager;
+	} android ;
+#endif
+
 };
 
 static int _impl_on_output_event(rh_asmp_itf self, rh_output_event_enum_t ev) {
@@ -71,6 +100,8 @@ static int _impl_open(rh_asmp_itf self, const char * const fn) {
 	{
 		if(strncmp("rh_rawpak_ctx://",fn,16)==0) {
 
+			/* READ audio from a rawpak container, either on disk, or android APK */
+
 			void * p = NULL;
 
 			if(sscanf(fn+16,"%p",&p) != 1) {
@@ -83,7 +114,29 @@ static int _impl_open(rh_asmp_itf self, const char * const fn) {
 				return -1;
 			}
 		}
+#ifdef __ANDROID__
+		else if(strncmp("apk://",fn,6) == 0) {
+
+			/* READ audio from a file in the android APK */
+
+			/*** MEGGA HACK! ***/
+			extern AAssetManager * __rh_hack_get_android_asset_manager();
+
+			instance->android.assetManager = __rh_hack_get_android_asset_manager();
+			instance->android.asset = AAssetManager_open( instance->android.assetManager, fn+6, AASSET_MODE_RANDOM);
+
+			if(instance->android.asset && apk_open_avformatctx(self, 0, &(instance->pFormatCtx) ) != 0) {
+
+				AAsset_close(instance->android.asset);
+				instance->android.asset = NULL;
+				instance->android.assetManager = NULL;
+				return -1;
+			}
+		}
+#endif /*** __ANDROID__ ***/
 		else {
+
+			/* READ audio from a file on the disk */
 
 			if(avformat_open_input(&instance->pFormatCtx, fn, NULL, NULL)!=0) {
 				av_freep(&instance->pFrame);
@@ -157,13 +210,39 @@ static int _impl_open(rh_asmp_itf self, const char * const fn) {
     	break;
     }
 
-	if( instance->pCodecCtx->sample_fmt != AV_SAMPLE_FMT_S16 )
+	if( instance->pCodecCtx->sample_fmt != AV_SAMPLE_FMT_S16 ) {
+
 		printf("ERROR: audio native format is not S16, we will have to re-sample (TODO)\n");
+		avformat_close_input(&instance->pFormatCtx);
+		av_freep(&instance->pFrame);
+
+		return -1;
+	}
 
     return 0;
   }
 
   return -1;
+}
+
+static int _impl_openf(rh_asmp_itf self, const char * const format, ...) {
+
+	int err = 0;
+	char *path = NULL;
+	va_list va;
+	va_start(va, format);
+    if(!((path = malloc(sizeof (char) * PATH_MAX))))
+       err = -1;
+    else if(vsnprintf(path,PATH_MAX,format,va)>=PATH_MAX)
+        err = -1; /* truncated */
+    va_end(va);
+
+	if(!err)
+		err = _impl_open(self, path);
+
+	free(path);
+
+	return err;
 }
 
 static int _ff_read_packet(rh_asmp_itf self) {
@@ -258,6 +337,11 @@ static int _impl_close(rh_asmp_itf *pself) {
 		if(instance->pFormatCtx) 	avformat_close_input( &instance->pFormatCtx );
 		if(instance->pFrame)		av_freep(&instance->pFrame);
 
+#ifdef __ANDROID__
+		if(instance->android.asset)
+			AAsset_close(instance->android.asset);
+#endif
+
 		free( instance->interface    );
 		free( instance );
 	  }
@@ -326,6 +410,7 @@ int rh_asmp_create_ffmpeg( rh_asmp_itf * itf, asmp_cb_func_t cb_func, void * cb_
 		instance->cb_data = cb_data;
 
 		interface->open  		= &_impl_open;
+		interface->openf        = &_impl_openf;
 		interface->addref       = &_impl_addref;
 		interface->reset		= &_impl_reset;
 		interface->read			= &_impl_read;
@@ -336,7 +421,7 @@ int rh_asmp_create_ffmpeg( rh_asmp_itf * itf, asmp_cb_func_t cb_func, void * cb_
 		interface->channels		= &_impl_channels;
 
 		interface->on_output_event = &_impl_on_output_event;
-		
+
 		*itf = (rh_asmp_itf)instance;
 
 		return 0;
@@ -344,4 +429,119 @@ int rh_asmp_create_ffmpeg( rh_asmp_itf * itf, asmp_cb_func_t cb_func, void * cb_
 
 	return -1;
 }
+
+
+/*************************************** PLAY AUDIO FROM A FILE INSIDE AN APK ***************************************/
+
+#ifdef __ANDROID__
+
+static int apk_read_func(void* ptr, uint8_t* buf, int buf_size)
+{
+	rh_asmp_itf self = (rh_asmp_itf)ptr;
+
+	struct asmp_instance * instance = (struct asmp_instance *)(self);
+
+	int e = AAsset_read(instance->android.asset, buf, buf_size);
+
+	return (e >= 0) ? e : -1;
+}
+
+static int64_t apk_seek_func(void* ptr, int64_t pos, int whence)
+{
+	rh_asmp_itf self = (rh_asmp_itf)ptr;
+
+	struct asmp_instance * instance = (struct asmp_instance *)(self);
+
+	if(whence == AVSEEK_SIZE)
+		return AAsset_getLength(instance->android.asset);
+
+	return AAsset_seek(instance->android.asset, pos, whence);
+}
+
+static int apk_open_avformatctx(rh_asmp_itf self, size_t internalBufferSize, AVFormatContext ** format_ctx ) {
+
+	struct asmp_instance * instance = (struct asmp_instance *)(self);
+
+	AVFormatContext* pCtx = NULL;
+	AVIOContext* pIOCtx;
+	int probedBytes = 0;
+
+	if(!instance || !format_ctx)
+		return -1;
+
+	instance->android.avformat_buffer_size = internalBufferSize ? internalBufferSize : (32 * 1024); // 32k default.
+
+	if((instance->android.avformat_buffer = calloc(1, instance->android.avformat_buffer_size)) == NULL)
+		return -1;
+
+	pIOCtx = avio_alloc_context(
+		(unsigned char*)instance->android.avformat_buffer, instance->android.avformat_buffer_size,
+		0,
+		self,
+		&apk_read_func,
+		NULL,
+		&apk_seek_func);
+
+	if(!pIOCtx) {
+		free(instance->android.avformat_buffer);
+		instance->android.avformat_buffer = NULL;
+		instance->android.avformat_buffer_size = 0;
+		return -1;
+	}
+
+	pCtx = avformat_alloc_context();
+
+	if(!pCtx) {
+		av_free(pIOCtx);
+		free(instance->android.avformat_buffer);
+		instance->android.avformat_buffer = NULL;
+		instance->android.avformat_buffer_size = 0;
+		return -1;
+	}
+
+	// Set the IOContext:
+	pCtx->pb = pIOCtx;
+
+	// Determining the input format:
+	{
+		size_t filesize = AAsset_getLength(instance->android.asset);
+		probedBytes = filesize > instance->android.avformat_buffer_size ? instance->android.avformat_buffer_size : filesize;
+	}
+
+	if(AAsset_read(instance->android.asset, instance->android.avformat_buffer, probedBytes) < 0) {
+
+		av_free(pIOCtx);
+		avformat_free_context(pCtx);
+		free(instance->android.avformat_buffer);
+		instance->android.avformat_buffer = NULL;
+		instance->android.avformat_buffer_size = 0;
+		return -1;
+	}
+
+	AAsset_seek(instance->android.asset, 0, SEEK_SET); // cannot fail.
+
+	AVProbeData probeData;
+	probeData.buf = (unsigned char*)instance->android.avformat_buffer;
+	probeData.buf_size = probedBytes;
+	probeData.filename = "";
+
+	pCtx->iformat = av_probe_input_format(&probeData, 1);
+
+	pCtx->flags = AVFMT_FLAG_CUSTOM_IO;
+
+	if((avformat_open_input(&pCtx, "", 0, 0)) != 0) {
+		av_free(pIOCtx);
+		avformat_free_context(pCtx);
+		free(instance->android.avformat_buffer);
+		instance->android.avformat_buffer = NULL;
+		instance->android.avformat_buffer_size = 0;
+		return -1;
+	}
+
+	*format_ctx = pCtx;
+
+	return 0;
+}
+
+#endif /*** __ANDROID__ **/
 
